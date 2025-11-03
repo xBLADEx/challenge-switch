1b3d49ef291e1ee4c8beeec98a2627ffb985a95c
Rich Edmunds <xBLADEx@users.noreply.github.com>
2025-11-03 22:55:59 +0000

    feat: Add data, instructions, and prettier

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config} */
+const prettierConfig = {
+  $schema: "https://json.schemastore.org/prettierrc",
+  printWidth: 100,
+  singleQuote: true,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default prettierConfig;
